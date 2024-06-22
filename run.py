a8b6fbdcf8c522348a4ef02cbfd8aaa3de54b1e0
Serhii Panov <serhiipanovjs@gmail.com>
2024-06-22 20:46:23 +0100

    added essential libs dependencies

--- run.py
+++ run.py
@@ -1,3 +1,14 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import gspread
+from google.oauth2.service_account import Credentials
+
+import colorama
+from colorama import Fore, Back, Style
+import sys
+import time
+import random
+
+import pyperclip
+
+colorama.init(autoreset=True)
+
+
