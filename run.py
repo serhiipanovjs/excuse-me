e67a14dc10f4af4235439235c6f2d7e02ee70e42
Serhii Panov <serhiipanovjs@gmail.com>
2024-06-22 21:03:08 +0100

    connected gspread to application sheet

--- run.py
+++ run.py
@@ -11,4 +11,13 @@ import pyperclip
 
 colorama.init(autoreset=True)
 
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
 
+CREDENTIALS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDENTIALS = CREDENTIALS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDENTIALS)
+SHEET = GSPREAD_CLIENT.open('my_excuse_sheet')
