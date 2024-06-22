2c0585a397001e0b9857dfbde5408733b72c7ea2
Serhii Panov <serhiipanovjs@gmail.com>
2024-06-22 21:28:39 +0100

    added custom functions

--- run.py
+++ run.py
@@ -21,3 +21,20 @@ CREDENTIALS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDENTIALS = CREDENTIALS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDENTIALS)
 SHEET = GSPREAD_CLIENT.open('my_excuse_sheet')
+
+def clear_terminal():
+    print(end="\033c", flush=True)
+
+def animated_print(text):
+    for character in text:
+        sys.stdout.write(character)
+        sys.stdout.flush()
+        time.sleep(0.05)
+
+
+def animated_input(text):
+    for character in text:
+        sys.stdout.write(character)
+        sys.stdout.flush()
+        time.sleep(0.05)
+    return input()
